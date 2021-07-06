8bfb26a26fd71c00cfb1c76ad9c8effbb2d2a546
Jon Fairbanks <fairplay89@gmail.com>
2021-07-05 18:43:10 -0700

    I still totally hate it

--- pages/api/forecast.ts
+++ pages/api/forecast.ts
@@ -1,29 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { useRouter } from 'next/router'
+export default function handler(req, res) {
+  let apiKey;
+  if(process.env.OPEN_WEATHER_API_KEY !== undefined || process.env.OPEN_WEATHER_API_KEY !== "undefined"){
+    apiKey = process.env.OPEN_WEATHER_API_KEY;
+  }else{
+    new Error("OpenWeather API Key not found!");
+  }
+  let { lat, long } = req.query
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
 
-const router = useRouter()
-const { lat, long } = router.query
-
-let apiKey;
-if(process.env.OPEN_WEATHER_API_KEY !== undefined || process.env.OPEN_WEATHER_API_KEY !== "undefined"){
-  apiKey = process.env.OPEN_WEATHER_API_KEY;
-}else{
-  new Error("OpenWeather API Key not found!");
-}
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
-console.log('API Url: ' + apiUrl);
-let response = fetch(apiUrl);
-console.log('Resp: ' + response);
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Fuck Typescript' })
+  async function fetchForecastJSON() {
+    const response = await fetch(apiUrl);
+    const forecast = await response.json();
+    return forecast;
+  }
+  
+  fetchForecastJSON().then(forecast => {
+    res.status(200).json(forecast)
+  });
 }
