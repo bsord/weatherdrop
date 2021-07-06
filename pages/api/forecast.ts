bd290d045187ff45eb93b58103baa7b7547f59a7
Jon Fairbanks <fairplay89@gmail.com>
2021-07-05 18:22:15 -0700

    Update forecast.ts

--- pages/api/forecast.ts
+++ pages/api/forecast.ts
@@ -2,6 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { useRouter } from 'next/router'
 
+const router = useRouter()
+const { lat, long } = router.query
+
 let apiKey;
 if(process.env.OPEN_WEATHER_API_KEY !== undefined || process.env.OPEN_WEATHER_API_KEY !== "undefined"){
   apiKey = process.env.OPEN_WEATHER_API_KEY;
@@ -22,5 +25,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Hello World' })
+  res.status(200).json({ name: 'Fuck Typescript' })
 }
