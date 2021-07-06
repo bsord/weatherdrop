1a566eb584b37f0c505ddc2e73aa05019522984d
Jon Fairbanks <fairplay89@gmail.com>
2021-07-06 16:52:44 -0700

    Fix error handling

--- pages/api/forecast.js
+++ pages/api/forecast.js
@@ -24,30 +24,24 @@ export default function handler(req, res) {
     }
   }
 
-  try {
-    fetchForecastJSON().then(forecast => {
-      if('cod' in forecast && forecast.cod == '200'){
-        res.status(200).json({
-          city: forecast.name,
-          city_id: forecast.id,
-          temp_actual: forecast.main.temp,
-          temp_feels: forecast.main.feels_like,
-          temp_min: forecast.main.temp_min,
-          temp_max: forecast.main.temp_max,
-          weather_status: forecast.weather[0].main,
-          weather_status_desc: forecast.weather[0].description,
-          weather_status_id: forecast.weather[0].id,
-        })
-      } else {
-        res.status(500).json({
-          message: "There was a problem with the weather data"
-        })
-      }
-    });
-  } catch(err) {
+  fetchForecastJSON().then(forecast => {
+    if('cod' in forecast && forecast.cod == '200'){
+      res.status(200).json({
+        city: forecast.name,
+        city_id: forecast.id,
+        temp_actual: forecast.main.temp,
+        temp_feels: forecast.main.feels_like,
+        temp_min: forecast.main.temp_min,
+        temp_max: forecast.main.temp_max,
+        weather_status: forecast.weather[0].main,
+        weather_status_desc: forecast.weather[0].description,
+        weather_status_id: forecast.weather[0].id,
+      })
+    } else { throw new Error() }
+  }).catch(err => {
+    console.log(err)
     res.status(500).json({
       message: "There was a problem parsing weather data"
     })
-    console.log(err)
-  }
+  });
 }
