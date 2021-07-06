0ab5d9c227fb79f0ef634b469b51e3ffc20b3a1b
Jon Fairbanks <fairplay89@gmail.com>
2021-07-05 18:14:28 -0700

    Fix this shit

--- pages/api/forecast.js
+++ pages/api/forecast.js
@@ -1,20 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 import { useRouter } from 'next/router'
 
 let apiKey;
 if(process.env.OPEN_WEATHER_API_KEY !== undefined || process.env.OPEN_WEATHER_API_KEY !== "undefined"){
   apiKey = process.env.OPEN_WEATHER_API_KEY;
 }else{
-  new Error("OpenWeather API Key not found!")
-}
-
-type Data = {
-  response: string
+  new Error("OpenWeather API Key not found!");
 }
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
-console.log('API Url: ' + apiUrl)
+console.log('API Url: ' + apiUrl);
 let response = fetch(apiUrl);
 console.log('Resp: ' + response);
 
@@ -22,5 +18,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json(response)
+  res.status(200).json({name: "Fuck Typescript"})
 }
