47eaad610fc2662572d42a19a160af733543020f
Jon Fairbanks <fairplay89@gmail.com>
2021-07-07 16:40:16 -0700

    Lint Fixes

--- pages/location.js
+++ pages/location.js
@@ -59,7 +59,7 @@ export default function Location() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/css/all.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet" />
       </Head>
       <main className={styles.main}>
