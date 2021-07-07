740615faecb9c0da5a4ce73cd9dcf85643778a21
Jon Fairbanks <fairplay89@gmail.com>
2021-07-07 11:28:44 -0700

    Truncate Temp values

--- pages/api/forecast.js
+++ pages/api/forecast.js
@@ -1,5 +1,3 @@
-
-
 export default function handler(req, res) {
   let apiKey;
   if(process.env.OPEN_WEATHER_API_KEY !== undefined || process.env.OPEN_WEATHER_API_KEY !== "undefined"){
@@ -29,10 +27,10 @@ export default function handler(req, res) {
       res.status(200).json({
         city: forecast.name,
         city_id: forecast.id,
-        temp_actual: forecast.main.temp,
-        temp_feels: forecast.main.feels_like,
-        temp_low: forecast.main.temp_min,
-        temp_high: forecast.main.temp_max,
+        temp_actual: forecast.main.temp.toFixed(1),
+        temp_feels: forecast.main.feels_like.toFixed(1),
+        temp_low: forecast.main.temp_min.toFixed(1),
+        temp_high: forecast.main.temp_max.toFixed(1),
         weather_status: forecast.weather[0].main,
         weather_status_desc: forecast.weather[0].description,
         weather_status_id: forecast.weather[0].id,
